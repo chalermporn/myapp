cccfc9bf1fc77c1efef78a259a8e2ed23bb754ea
Chalermporn Posoppitakwong <chalermporn.posoppitakwong@krungthai.com>
2024-02-16 00:43:04 +0700

    up

--- myapp.rb
+++ myapp.rb
@@ -1,7 +1,7 @@
 class Myapp < Formula
     desc "My App Description"
     homepage "https://github.com/chalermporn/myapp"
-    url "https://github.com/chalermporn/myapp/archive/refs/tags/1.0.tar.gz"
+    url "https://github.com/chalermporn/myapp/archive/refs/tags/1.0.1.tar.gz"
     sha256 "your_sha256_checksum_here"
   
     def install
