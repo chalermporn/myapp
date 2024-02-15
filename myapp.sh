f905b73daf9ab528e79f7a163b65070fde39cc62
Chalermporn Posoppitakwong <chalermporn.posoppitakwong@krungthai.com>
2024-02-16 00:42:33 +0700

    fix

--- myapp.sh
+++ myapp.sh
@@ -1,3 +1,15 @@
 #!/bin/bash
-echo "Hello, this is my app!"
-# ln -s /Users/bird/BASH/lean/myapp.sh /usr/local/bin/myapp
+
+# Include the func.sh file
+source func.sh
+
+log "Script started."
+
+# Rest of the script
+
+
+options=("Option 1" "Option 2" "Option 3" "Option 4")
+get_user_input "Select an option:" "${options[@]}"
+selected_index=$?
+
+echo "You selected: ${options[$selected_index]}"
